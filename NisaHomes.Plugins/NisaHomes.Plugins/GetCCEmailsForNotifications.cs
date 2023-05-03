8e5bbba5741faa1b964367e7cf9705fa5420a262
Ezequiel Baravalle <ezebaravalle@gmail.com>
2023-05-02 22:49:08 -0300

    Updates

---
+++ NisaHomes.Plugins/NisaHomes.Plugins/GetCCEmailsForNotifications.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NisaHomes.Plugins
+{
+    public class GetCCEmailsForNotifications: IPlugin
+    {
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracer = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = factory.CreateOrganizationService(context.UserId);
+
+
+            try
+            {
+
+                EntityReference targetEntity = null;
+                EntityReference relatedEntity = null;
+                Entity FinancialRequest = null;
+                Entity Users = null;
+                string relationshipname = "";
+
+                if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is EntityReference)
+                {
+
+
+
+                    targetEntity = (EntityReference)context.InputParameters["Target"];
+
+                }
+                if (context.InputParameters.Contains("Relationship"))
+                {
+
+                    relationshipname = ((Relationship)context.InputParameters["Relationship"]).SchemaName;
+
+                }
+
+                if (relationshipname != "cr566_cr566_financialassistancerequest_systemus")
+                {
+                    return;
+                }
+                if (context.InputParameters.Contains("RelatedEntities") && context.InputParameters["RelatedEntities"] is EntityReferenceCollection)
+                {
+
+                    EntityReferenceCollection relatedEntitieCollection = (EntityReferenceCollection)context.InputParameters["RelatedEntities"];
+                    relatedEntity = relatedEntitieCollection[0];
+                }
+
+                FinancialRequest = service.Retrieve(targetEntity.LogicalName, targetEntity.Id, new ColumnSet("cr566_ccnotificationemails"));
+                Users = service.Retrieve(relatedEntity.LogicalName, relatedEntity.Id, new ColumnSet(false));
+
+                //Retrieve contacts emails related to FinancialRequest
+                QueryExpression query = new QueryExpression("cr566_cr566_financialassistancerequest_system");
+                query.Criteria.AddCondition("cr566_financialassistancerequestid", ConditionOperator.Equal, FinancialRequest.Id);
+                query.ColumnSet = new ColumnSet("systemuserid");
+                EntityCollection results = service.RetrieveMultiple(query);
+                string CCForNotificationsEmails = "";
+                foreach (Entity result in results.Entities)
+                {
+                    //Take contactid and search its email 
+                    Guid userguid = result.GetAttributeValue<Guid>("systemuserid");
+                    Entity user = service.Retrieve("systemuser", userguid, new ColumnSet("internalemailaddress"));
+
+                    CCForNotificationsEmails += user.GetAttributeValue<String>("internalemailaddress") + ";";
+
+                }
+
+
+                if (context.MessageName.ToLower() == "associate" || context.MessageName.ToLower() == "disassociate")
+                {
+
+                    FinancialRequest.Attributes["cr566_ccnotificationemails"] = CCForNotificationsEmails;
+                    service.Update(FinancialRequest);
+
+                }
+
+
+
+            }
+            catch (Exception e)
+            {
+                throw new InvalidPluginExecutionException("Error Details : " + e.Message);
+            }
+        }
+
+    }
+}
--- NisaHomes.Plugins/NisaHomes.Plugins/GetSecondaryApproversEmailsMultiLevelApprovalSettings.cs
+++ NisaHomes.Plugins/NisaHomes.Plugins/GetSecondaryApproversEmailsMultiLevelApprovalSettings.cs
@@ -1,8 +1,10 @@
 ﻿namespace NisaHomes.Plugins
 {
     using Microsoft.Xrm.Sdk;
+    using Microsoft.Xrm.Sdk.Query;
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
@@ -15,15 +17,72 @@
             IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
             IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
             IOrganizationService service = factory.CreateOrganizationService(context.UserId);
-            Entity Target = (Entity)context.InputParameters["Target"];
-
+       
+            
             try
             {
+
+                EntityReference targetEntity = null;
+                EntityReference relatedEntity = null;
+                Entity MultiLevelSettings = null;
+                Entity Users = null;
+                string relationshipname = "";
+
+                if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is EntityReference) {
+
+      
+
+                    targetEntity = (EntityReference) context.InputParameters["Target"];
+                
+                }
+                if (context.InputParameters.Contains("Relationship")) {
+
+                    relationshipname = ((Relationship)context.InputParameters["Relationship"]).SchemaName;
+                   
+                }
+
+                if (relationshipname != "cr566_cr566_multilevelapprovalsettings_systemus") {
+                    return;
+                }
+                if (context.InputParameters.Contains("RelatedEntities") && context.InputParameters["RelatedEntities"] is EntityReferenceCollection) {
+
+                    EntityReferenceCollection relatedEntitieCollection = (EntityReferenceCollection) context.InputParameters["RelatedEntities"];
+                    relatedEntity = relatedEntitieCollection[0];
+                }
+
+                MultiLevelSettings = service.Retrieve(targetEntity.LogicalName, targetEntity.Id, new ColumnSet("cr566_secondaryapproversemails"));
+                Users = service.Retrieve(relatedEntity.LogicalName, relatedEntity.Id, new ColumnSet(false));
+
+                //Retrieve contacts emails related to Multilevelsettings
+                QueryExpression query = new QueryExpression("cr566_cr566_multilevelapprovalsettings_system");
+                query.Criteria.AddCondition("cr566_multilevelapprovalsettingsid", ConditionOperator.Equal, MultiLevelSettings.Id);
+                query.ColumnSet = new ColumnSet("systemuserid");
+                EntityCollection results = service.RetrieveMultiple(query);
+                string secondaryApproversEmails = "";
+                foreach (Entity result in results.Entities)
+                {
+                   //Take contactid and search its email 
+                    Guid userguid = result.GetAttributeValue<Guid>("systemuserid");
+                    Entity user = service.Retrieve("systemuser",userguid , new ColumnSet("internalemailaddress"));
+                   
+                    secondaryApproversEmails += user.GetAttributeValue<String>("internalemailaddress") + ";";
+                    
+                }
+
+
+                if (context.MessageName.ToLower() == "associate" || context.MessageName.ToLower() == "disassociate") {
+                    
+                    MultiLevelSettings.Attributes["cr566_secondaryapproversemails"] = secondaryApproversEmails;
+                    service.Update(MultiLevelSettings);
+
+                }
+
+              
 
             }
             catch (Exception e)
             {
-                throw new InvalidPluginExecutionException("Mensaje de error" + e.Message);
+                throw new InvalidPluginExecutionException("Error Details : " + e.Message);
             }
         }
     }
